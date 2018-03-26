d88137f9dc145710b2b41e8cc9773f8c6e02250c
Lisa Steele <lisahsteele@gmail.com>
2018-03-26 14:13:16 -0700

    Added animation for white bar to slide to position under the selected menu item.

--- MyWorldCup/MenuBar.swift
+++ MyWorldCup/MenuBar.swift
@@ -37,16 +37,26 @@ class MenuBar: UIView, UICollectionViewDataSource, UICollectionViewDelegate, UIC
         setupHorizontalBar()
     }
     
+    var horizontalBarLeftAnchorConstraint : NSLayoutConstraint?
+    
     func setupHorizontalBar() {
         let horizontalBarView = UIView()
         horizontalBarView.translatesAutoresizingMaskIntoConstraints = false
         horizontalBarView.backgroundColor = UIColor.white
         addSubview(horizontalBarView)
         
-        horizontalBarView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        horizontalBarLeftAnchorConstraint = horizontalBarView.leftAnchor.constraint(equalTo: self.leftAnchor)
+        horizontalBarLeftAnchorConstraint?.isActive = true
         horizontalBarView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
         horizontalBarView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 1/4).isActive = true
-        horizontalBarView.heightAnchor.constraint(equalToConstant: 5).isActive = true 
+        horizontalBarView.heightAnchor.constraint(equalToConstant: 5).isActive = true
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let x = CGFloat(indexPath.item) * frame.width / 4
+        horizontalBarLeftAnchorConstraint?.constant = x
+        
+        UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: self.layoutIfNeeded, completion: nil)
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
